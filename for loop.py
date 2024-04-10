d088ff255bf2e1aa434fadd6db5136520140fdd5
Dhruv <145555897+imdhruv1602@users.noreply.github.com>
2024-04-10 14:34:59 +0530

    Update for loop.py

--- for loop.py
+++ for loop.py
@@ -1,92 +1,92 @@
-# # 4-1
-# pizzas=["cheese and corn pizza", "peppriono", "onion"]
-# for pizza in pizzas:
-#     print(pizza)
+# 4-1
+pizzas=["cheese and corn pizza", "peppriono", "onion"]
+for pizza in pizzas:
+    print(pizza)
 
-# pizzas=["cheese and corn pizza", "peppriono", "onion"]
-# for pizza in pizzas:
-#     print(f"I like {pizza}")
+pizzas=["cheese and corn pizza", "peppriono", "onion"]
+for pizza in pizzas:
+    print(f"I like {pizza}")
 
-# print("I like cheese and corn pizza very much")
-# print("The topings are quite tastier than any other pizza")
-# print("It is affordable")
-# print("I really love pizza")
+print("I like cheese and corn pizza very much")
+print("The topings are quite tastier than any other pizza")
+print("It is affordable")
+print("I really love pizza")
 
-# # 4-2
-# animals=["cow", "buffalo", "goat"]
-# for animal in animals:
-#     print(animal)
+# 4-2
+animals=["cow", "buffalo", "goat"]
+for animal in animals:
+    print(animal)
 
-# animals=["cow", "buffalo", "goat"]
-# for animal in animals:
-#     print(f"A {animal} would make a great pet")
+animals=["cow", "buffalo", "goat"]
+for animal in animals:
+    print(f"A {animal} would make a great pet")
 
-# print("They all give milk and any of these will make a great pet")
+print("They all give milk and any of these will make a great pet")
 
-# # 4-3
-# for i in range(1,21):
-#     print(i)
+# 4-3
+for i in range(1,21):
+    print(i)
 
-# # 4-4
-# for i in range(1,1000000):
-#     print(i)
+# 4-4
+for i in range(1,1000000):
+    print(i)
 
-# # 4-5
-# n=list(range(1,1000001))
-# print(min(n))
-# print(max(n))
-# print(sum(n))
+# 4-5
+n=list(range(1,1000001))
+print(min(n))
+print(max(n))
+print(sum(n))
 
-# # 4-6
-# odds=list(range(1,31,2))
-# for odd in odds:
-#     print(odd)
+# 4-6
+odds=list(range(1,31,2))
+for odd in odds:
+    print(odd)
 
-# # 4-7
-# multiples=[3,6,9,12,15,18,21,24,27,30]
-# for multiple in multiples:
-#     print(multiple)
+# 4-7
+multiples=[3,6,9,12,15,18,21,24,27,30]
+for multiple in multiples:
+    print(multiple)
 
-# # 4-8
-# cubes=[]
-# for value in range(1,11):
-#     cube=value**3
-#     cubes.append(cube)
+# 4-8
+cubes=[]
+for value in range(1,11):
+    cube=value**3
+    cubes.append(cube)
 
-# for cube in cubes:
-#     print(cube)
+for cube in cubes:
+    print(cube)
 
-# # 4-9
-# cubes=[value ** 3 for value in range(1,11)]
-# print(cubes)
+# 4-9
+cubes=[value ** 3 for value in range(1,11)]
+print(cubes)
 
-# # 4-10
-# favourite_places=["Goa", "Udaipur", "Uttarakhand", "Rajasthan", "Vrindavan"]
-# print("The first three items in the list are: ")
-# print(favourite_places[0:3])
+# 4-10
+favourite_places=["Goa", "Udaipur", "Uttarakhand", "Rajasthan", "Vrindavan"]
+print("The first three items in the list are: ")
+print(favourite_places[0:3])
 
-# print("The middle three items in the list are: ")
-# print(favourite_places[1:4])
+print("The middle three items in the list are: ")
+print(favourite_places[1:4])
 
-# print("The last three items in the list are: ")
-# print(favourite_places[-3:])
+print("The last three items in the list are: ")
+print(favourite_places[-3:])
 
-# 4-11
-# my_pizzas=["cheese and corn pizza", "peppriono", "onion"]
-# your_pizzas = my_pizzas[:]
+4-11
+my_pizzas=["cheese and corn pizza", "peppriono", "onion"]
+your_pizzas = my_pizzas[:]
 
-# my_pizzas.append("farmhouse")
-# your_pizzas.append("punjabi tadka")
+my_pizzas.append("farmhouse")
+your_pizzas.append("punjabi tadka")
 
-# print("My favourite pizzas are: ")
-# print(my_pizzas)
-# for my_pizza in my_pizzas:
-#     print(my_pizza)
+print("My favourite pizzas are: ")
+print(my_pizzas)
+for my_pizza in my_pizzas:
+    print(my_pizza)
 
-# print("Your favourite pizzas are: ")
-# print(your_pizzas)
-# for your_pizza in your_pizzas:
-#     print(your_pizza)
+print("Your favourite pizzas are: ")
+print(your_pizzas)
+for your_pizza in your_pizzas:
+    print(your_pizza)
 
 # 4-12
 restraunts=('burger', 'pizza', 'momos', 'french fries')
